289767f13f2446c4df09fd01172d0374beaa3e85
SolvedGames <saket08tamrakar@gmail.com>
2023-05-05 14:57:24 +0530

    Added basic notes functionality

--- static/js/notes.js
+++ static/js/notes.js
@@ -5,10 +5,32 @@ var prevMousedownNoteLocation = [2, 3];
 var updateNoteLocationInterval = -1;
 
 var currentMovingNote = null;
+var currentHoveringNote = null;
+
+var currentEditingConnectionIndex = -1;
+
+var updateNoteConnectionInterval = -1;
+
+var connections = [
+    
+]
 
 function updateNoteEvents()
 {
     $(".note .note-textarea").off();
+
+    $(".note").on("mouseenter", (e) =>
+    {
+        $(e.currentTarget).data("isHovering", true);
+        currentHoveringNote = e.currentTarget;
+    });
+
+    $(".note").on("mouseleave", (e) =>
+    {
+        $(e.currentTarget).data("isHovering", false);
+        currentHoveringNote = null;
+
+    }).data("isHovering", false);
 
     $(".note .note-textarea").on("mousedown", (e) =>
     {
@@ -27,6 +49,8 @@ function updateNoteEvents()
             updateNoteLocationInterval = setInterval(() =>
             {
                 updateNoteLocationToMouse(note, [offsetX, offsetY]);
+
+                updateConnectionLocationOnNoteMove(note);
         
             }, 0);
         }
@@ -57,9 +81,20 @@ function updateNoteEvents()
     {
         var text = e.target.innerText;
         
-        if (text == "")
+        if (text == "" && (e.code == "Backspace" || e.code == "Delete"))
         {
             $(e.target).remove();
+        }
+    });
+
+
+    $(".note .note-connection-icon").off();
+
+    $(".note .note-connection-icon").on("mousemove", (e) =>
+    {
+        if (isMouseDown && updateNoteConnectionInterval == -1)
+        {
+            addNewNoteConnection(e.target.parentElement);
         }
     });
 }
@@ -76,17 +111,31 @@ $(document).on("mousedown", () =>
 
 $(document).on("mouseup", () =>
 {
-    currentMovingNote.css("z-index", "");
-    currentMovingNote = null;
+    if (currentMovingNote)
+    {
+        currentMovingNote.css("z-index", "");
+        currentMovingNote = null;
+    }
 
     clearInterval(updateNoteLocationInterval);
     updateNoteLocationInterval = -1;
-});
 
-$(document).on("mouseup", () =>
-{
+    if (updateNoteConnectionInterval != -1)
+    {
+        clearInterval(updateNoteConnectionInterval);
+
+        var connection = connections[currentEditingConnectionIndex];
+        if (currentHoveringNote != null && currentHoveringNote != connection.note)
+        {
+            connections[currentEditingConnectionIndex].connectedNote = currentHoveringNote;
+        }
+
+        updateNoteConnectionInterval = -1;
+    }
+
     isMouseDown = false;
 });
+
 
 function updateNoteLocationToMouse(note, offset)
 {
@@ -95,6 +144,91 @@ function updateNoteLocationToMouse(note, offset)
 
     //console.log(mouseLocation[0] + " : " + mouseLocation[1]);
     //console.log(offset[0] + " : " + offset[1]);
+}
+
+function updateConnectionLocationToMouse(note)
+{
+    var index = -1;
+
+    for (let i = 0; i < connections.length; i++)
+    {
+        const connection = connections[i];
+        
+        if (connection.note == note)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    var connection = connections[index];
+
+    var path = connection.path;
+    var points = connection.points;
+
+    var noteHalfWidth = $(note).width() / 2;
+    var noteHalfHeight = $(note).height() / 2;
+
+    var posX = Number($(note).css("left").replace("px", ""));
+    var posY = Number($(note).css("top").replace("px", ""));
+
+    const p0 = { x: Number($(note).css("left").replace("px", "")) + $(note).width() / 2, y: Number($(note).css("top").replace("px", "")) + $(note).height() / 2 };
+    const p2 = { x: mouseLocation[0], y: mouseLocation[1] };
+    const p1 = { x: Math.max(p0.x, p2.x) + (p0.x - p2.x) / 2, y: Math.max(p0.y, p2.y) + (p0.y - p2.y) / 2 };
+
+    connections[index].points = [p0, p1, p2];
+
+    path.setAttribute("d", "M" + (posX + noteHalfWidth) + "," + (posY + noteHalfHeight) + " " + (p2.x) + "," + (p2.y));
+}
+
+function updateConnectionLocationOnNoteMove(note)
+{
+    var index = -1;
+
+    for (let i = 0; i < connections.length; i++)
+    {
+        const connection = connections[i];
+        
+        if (connection.note.isEqualNode($(note).get(0)) || connection.connectedNote.isEqualNode($(note).get(0)))
+        {
+            index = i;
+            
+            var note = connection.note;
+            var connectedNote = connection.connectedNote;
+
+            var path = connection.path;
+            var points = connection.points;
+
+            var noteHalfWidth = $(note).width() / 2;
+            var noteHalfHeight = $(note).height() / 2;
+
+            var posX = Number($(note).css("left").replace("px", ""));
+            var posY = Number($(note).css("top").replace("px", ""));
+
+            const p0 = { x: Number($(note).css("left").replace("px", "")) + $(note).width() / 2, y: Number($(note).css("top").replace("px", "")) + $(note).height() / 2 };
+            var p2;
+            if (connectedNote == null)
+            {
+                p2 = points[2];
+            }
+            else
+            {
+                p2 = { x: Number($(connectedNote).css("left").replace("px", "")) + $(connectedNote).width() / 2, y: Number($(connectedNote).css("top").replace("px", "")) + $(connectedNote).height() / 2 };
+            }
+            const p1 = { x: Math.max(p0.x, p2.x) + (p0.x - p2.x) / 2, y: Math.max(p0.y, p2.y) + (p0.y - p2.y) / 2 };
+
+            connections[index].points = [p0, p1, p2];
+
+            path.setAttribute("d", "M" + (posX + noteHalfWidth) + "," + (posY + noteHalfHeight) + " " + (p2.x) + "," + (p2.y));
+        }
+    }
+
+    
+    //var connection = connections[index];
+    
+    
+
+    console.log(mouseLocation);
 }
 
 function addNewNote()
@@ -117,6 +251,39 @@ function addNewNote()
     }, 0);
 
     updateNoteEvents();
+}
+
+function addNewNoteConnection(note)
+{
+    var noteHalfWidth = $(note).width() / 2;
+    var noteHalfHeight = $(note).height() / 2;
+
+    var posX = Number($(note).css("left").replace("px", ""));
+    var posY = Number($(note).css("top").replace("px", ""));
+
+    const p0 = { x: Number($(note).css("left").replace("px", "")) + noteHalfWidth, y: Number($(note).css("top").replace("px", "")) + noteHalfHeight };
+    const p2 = { x: mouseLocation[0], y: mouseLocation[1] };
+    const p1 = { x: Math.max(p0.x, p2.x) + (p0.x - p2.x) / 2, y: Math.max(p0.y, p2.y) + (p0.y - p2.y) / 2 };
+
+    var svg = $("#connections-svg");
+
+    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M" + (posX + noteHalfWidth) + "," + (posY + noteHalfHeight) + " " + (p2.x) + "," + (p2.y));
+    path.setAttribute("stroke", "#fff");
+    path.setAttribute("stroke-width", "2");
+    path.setAttribute("fill", "none");
+    path.setAttribute("marker-end", "url(#arrow)");
+
+    svg.append(path);
+
+    connections.push({ note: note, connectedNote: null, path: path, points: [ p0, p1, p2 ]});
+
+    currentEditingConnectionIndex = connections.length - 1;
+    updateNoteConnectionInterval = setInterval(() =>
+    {
+        updateConnectionLocationToMouse(note);
+
+    }, 0);
 }
 
 $("#note-tool").on("mousemove", () =>
